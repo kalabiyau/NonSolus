283262a4e06cfdcaeea0e2d033a6bfed0ba546fb
Joshua Schmid <jschmid@suse.com>
2015-01-21 18:14:29 +0100

    add pending test for activities model

---
+++ spec/activity_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe 'Activity' do
+  it 'is not being saved without a name'
+  it 'will not be saved if there is a activity with the same name '
+end
