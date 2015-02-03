1b98331ae02e0559967dcc49403aae0244fff5db
Joshua Schmid <jschmid@suse.com>
2015-02-03 13:07:15 +0100

    add newline

--- spec/models/avtivity_spec.rb
+++ spec/models/avtivity_spec.rb
@@ -3,3 +3,4 @@ require 'rails_helper'
 describe Activity do
   it { should have_and_belong_to_many(:users) }
 end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,4 @@ require 'rails_helper'
 describe User do
   it { should have_and_belong_to_many(:activities) }
 end
+
