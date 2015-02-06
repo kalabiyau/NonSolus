579cd047198783c052edb2397be6e846fa10018a
Joshua Schmid <jschmid@suse.com>
2015-02-06 11:21:00 +0100

    remove duplication

--- db/migrate/20150202154317_create_join_table_activity_user.rb
+++ db/migrate/20150202154317_create_join_table_activity_user.rb
@@ -1,7 +1,6 @@
 class CreateJoinTableActivityUser < ActiveRecord::Migration
   def change
     create_join_table :activities, :users do |t|
-       t.index [:activity_id, :user_id]
        t.index [:user_id, :activity_id]
     end
   end
