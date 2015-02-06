c6ebd4798d7f597aedb04645d8f953bab57cebb8
Joshua Schmid <jschmid@suse.com>
2015-02-06 11:23:34 +0100

    Update 20150202154317_create_join_table_activity_user.rb

--- db/migrate/20150202154317_create_join_table_activity_user.rb
+++ db/migrate/20150202154317_create_join_table_activity_user.rb
@@ -1,8 +1,7 @@
 class CreateJoinTableActivityUser < ActiveRecord::Migration
   def change
     create_join_table :activities, :users do |t|
-       t.index [:activity_id, :user_id]
-       t.index [:user_id, :activity_id]
+      t.index [:user_id, :activity_id]
     end
   end
 end
