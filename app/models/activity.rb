c8164b03ce86d923189f5ae167d3a752b7020670
Thomas Hutterer <thutterer@suse.de>
2015-02-18 18:04:37 +0100

    Activity name validation
    closes #13

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,7 @@
 class Activity < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_one :creator, class_name: User
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
 
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 describe Activity do
+
   it { should have_and_belong_to_many(:users) }
   
-  it 'is not being saved without a name'
-  it 'will not be saved if there is a activity with the same name '
+  subject { create(:activity) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 
 end
