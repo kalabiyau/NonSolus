e8c3f15f287fe99816882f8c42e7f5f83bedae7a
Artem Chernikov <skullzeek@gmail.com>
2015-02-18 17:42:49 +0100

    Fix naming and move activity pending tests back to model

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Activity do
   it { should have_and_belong_to_many(:users) }
+  
+  it 'is not being saved without a name'
+  it 'will not be saved if there is a activity with the same name '
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
 require 'simplecov'
-require 'factory_girl_rails'
 
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
-
-  config.include FactoryGirl::Syntax::Methods
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
