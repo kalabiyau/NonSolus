2cd41d97c7f0fb821943c60c7d2091435da8accb
Thomas Hutterer <thutterer@suse.de>
2015-02-19 10:28:40 +0100

    use new format of validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,7 @@
 class Activity < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_one :creator, class_name: User
-  validates_presence_of :name
-  validates_uniqueness_of :name
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
 
