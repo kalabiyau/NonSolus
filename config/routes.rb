af583e9bce93ba3674cbea77182c084bcb1dd1bd
nwalter <nwalter@suse.de>
2015-02-20 13:36:08 +0100

    add route for joining activity

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   root 'application#index'
 
-  resources :activities
+  resources :activities do
+    member do
+      post :join
+    end
+  end
+
+  resources :users
 
   namespace :saml do
     get :init
