419c4ebb80bca2b025b5e0853cfd967dcd3f78be
Artem Chernikov <skullzeek@gmail.com>
2015-03-04 15:12:24 +0100

    Ensure proper return_path VERP

--- app/mailers/activities_mailer.rb
+++ app/mailers/activities_mailer.rb
@@ -3,7 +3,7 @@ class ActivitiesMailer < ApplicationMailer
   def new_activities_digest(user)
     @user = user
     @recent_activities = Activity.last(50)
-    mail(to: @user.email, subject: '[SUSE-ACT] New Activities Digest')
+    mail(to: @user.email, subject: '[SUSE-ACT] New Activities Digest', return_path: generate_verp_pattern)
   end
 
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'act@act.suse.de'
   layout 'mailer'
+
+  def generate_verp_pattern
+    return self.class.default[:from] unless @user
+    default_from = Mail::Address.new(self.class.default[:from])
+    verp = "#{default_from.local}+"
+    verp << [Mail::Address.new(@user.email).local, Mail::Address.new(@user.email).domain].join('=')
+    verp << "@#{default_from.domain}"
+  end
+
 end
