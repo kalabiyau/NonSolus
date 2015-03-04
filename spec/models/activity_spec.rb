f2cf84129b1f2e1b3b49f0f9a546a987d8aa6d30
nwalter <nwalter@suse.de>
2015-03-04 15:40:06 +0100

    validate presence of describtion for activity

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -5,6 +5,7 @@ describe Activity do
   it { should have_and_belong_to_many(:users) }
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
+  it { should validate_presence_of(:description)}
 
   describe '.send_new_activities_digest' do
 
