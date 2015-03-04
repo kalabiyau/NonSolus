930a1095626051a9ecdd30c4f0e33d2963a2b132
Artem Chernikov <skullzeek@gmail.com>
2015-03-04 13:13:19 +0100

    Set protocol to ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,10 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'act.suse.de' }
+  config.action_mailer.default_url_options = {
+    host: 'act.suse.de',
+    protocol: 'https'
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
