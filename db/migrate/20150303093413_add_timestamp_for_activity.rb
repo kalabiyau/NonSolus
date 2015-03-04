9c758c2f55387373797377784c3020d4e7dc9cd8
nwalter <nwalter@suse.de>
2015-03-04 15:36:48 +0100

    timestamp for activity

--- db/migrate/20150303093413_add_timestamp_for_activity.rb
+++ db/migrate/20150303093413_add_timestamp_for_activity.rb
@@ -2,9 +2,8 @@ class AddTimestampForActivity < ActiveRecord::Migration
   def change
     add_timestamps(:activities)
     Activity.find_each do |activity|
-      activity.created_at = Time.zone.now
-      activity.updated_at = Time.zone.now
-      activity.save!
+      activity.update_attribute(:updated_at, Time.zone.now)
+      activity.update_attribute(:created_at, Time.zone.now)
     end
   end
 end
