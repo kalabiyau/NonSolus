15c35358b6d88d314c511acd116a810f9bcbc386
Artem Chernikov <skullzeek@gmail.com>
2015-03-09 11:45:51 +0100

    Convert flash to flash now

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -13,11 +13,11 @@ class ActivitiesController < ApplicationController
   def create
     @activity = Activity.new(activity_params)
     unless @current_user
-      flash[:alert] = 'You need to be logged in to create an activity!'
+      flash.now.alert = 'You need to be logged in to create an activity!'
       render :new and return
     end
     @activity.creator = @current_user
-    flash[:notice] = 'Successfully created Activity' if @activity.save
+    flash.now.notice = 'Successfully created Activity' if @activity.save
     respond_with(@activity)
   end
 
@@ -33,10 +33,10 @@ class ActivitiesController < ApplicationController
   def update
     @activity = Activity.find(params[:id])
     if @activity.update_attributes(activity_params)
-      flash[:success] = 'Successfull updated Activity'
+      flash.now.notice = 'Successfull updated Activity'
       respond_with(@activity)
     else
-      flash[:alert] = 'Error editing Activity'
+      flash.now.alert = 'Error editing Activity'
       render :edit
     end
   end
@@ -50,11 +50,11 @@ class ActivitiesController < ApplicationController
   def join
     @activity = Activity.find(params[:id])
     unless @current_user
-      flash[:alert] = 'You need to be logged in to join an activity!'
+      flash.now.alert = 'You need to be logged in to join an activity!'
       redirect_to @activity and return
     end
     @current_user.activities << @activity
-    flash[:notice] = 'You joined the activity'
+    flash.now.notice = 'You joined the activity'
     redirect_to activities_url
   end
 
