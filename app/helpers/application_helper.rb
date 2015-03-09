4a2f66d206b914c7ecb767a12cdafb9348ce81b2
Artem Chernikov <skullzeek@gmail.com>
2015-03-09 12:54:33 +0100

    Colorize flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,19 @@ module ApplicationHelper
     @current_user.try(:decorate)
   end
 
+  def bootstrap_class_for(flash_type)
+    { success: 'alert-success', error: 'alert-danger', alert: 'alert-danger', notice: 'alert-info' }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(
+        content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} ") do
+          concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+          concat message
+        end
+      )
+    end
+    nil
+  end
 end
