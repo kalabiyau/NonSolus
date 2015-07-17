7961aa4760a6cf26f0b23724b4d222f1e8192c8f
Artem Chernikov <skullzeek@gmail.com>
2015-07-17 13:31:47 +0200

    Do not mail until there something to mail

--- app/mailers/activities_mailer.rb
+++ app/mailers/activities_mailer.rb
@@ -3,6 +3,7 @@ class ActivitiesMailer < ApplicationMailer
   def new_activities_digest(user)
     @user = user
     @recent_activities = Activity.where(created_at: 1.day.ago..Time.zone.now).last(50)
+    return unless @recent_activities.any?
     mail(to: @user.email, subject: '[SUSE-ACT] New Activities Digest', return_path: generate_verp_pattern)
   end
 
